e56e3bbd0dcf52c78b642241b3607a41d2ebe9e2
madmickey <michaeljcornell@gmail.com>
2026-05-21 13:56:45 +1000

    y

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 
-const CONFIG = {
-  rsvpEndpoint: "https://formspree.io/f/meedawqa",
-
-  venue: {
-    name: "Wedding Venue",
-    lat: -33.9108,
-    lng: 151.2027
-  },
-
-  wedding: {
-    date: "2026-10-10",
-    time: "3:00 PM"
-  },
-
-  contactEmail: "hello@wedding.com"
-};
-
 // RSVP
 document.getElementById("rsvpForm").addEventListener("submit", async (e) => {
   e.preventDefault();
